0509adb30472d3e3822e6f932d80b3cfa08a6ccc
magnomenezes <magnomenezes007@gmail.com>
2024-05-28 16:47:56 -0300

    testando commit2

--- conta_banco/src/conta_banco/ContaTerminal.java
+++ conta_banco/src/conta_banco/ContaTerminal.java
@@ -4,22 +4,27 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
+	
+
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
 		
-		 int Numero = 1021;
-		 String Agencia = 067-8;
-		String NomeDoCliente = Mario Andrade;
-		double Saldo = 247.48;
+		 int numero = 1021;
+		 //numero = sc.nextInt();
+		 double agencia = 067-8;
+		String nome = "Mario Andrade";
+		//String NomeDoCliente = Mario;
+		double saldo = 247.48;
 		
-		System.out.println("Por favor, digite o número da agencia:");
-		System.out.println("por favor, digite seu numero do cliente:");
-		System.out.println("Por favor, digite seu nome");
-		System.out.println("Olá" + NomeDoCliente + "," "obrigado por criar uma conta em nosso banco, sua agência é" + Agencia + ", conta" +
-				+ "   ");
+		System.out.println("Por favor, digite o número da agencia: ");
+		agencia = sc.nextDouble();
+		System.out.println("O numero da sua agencia é: " + agencia );
+		//System.out.println("por favor, digite seu numero do cliente:");
+		//System.out.println("Por favor, digite seu nome");
+		//System.out.println("Olá" + nome + ", Obrigado por criar uma conta Em nosso banco, sua agência é " + agencia);
 		
-
+		sc.close();
 	}
 
 }
